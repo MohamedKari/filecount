cf2ca44cadfdb7670a3c65005fa3ed2347f453d6
Mohamed Kari <mo@mkari.de>
2023-01-13 11:04:26 -0800

    refactor

--- filecount.py
+++ filecount.py
@@ -4,6 +4,47 @@ from pathlib import Path
 from argparse import ArgumentParser
 from typing import Iterable
 
+class Breakddown:
+    def __init__(self, 
+        counts_per_direct_child_dir: dict[Path, int], 
+        direct_child_files: list[Path]
+    ) -> None:
+        self.counts_per_direct_child_dir = counts_per_direct_child_dir
+        self.direct_child_files = direct_child_files
+    
+    def to_stdout(self) -> None:
+        print(f"{breakdown_dir}:")
+        for d_name, d_count in self.counts_per_direct_child_dir.items():
+            print(f"{d_count: 8}  {d_name}")
+        
+        f_count = len(self.direct_child_files)
+        f_name = "direct child files"
+        print(f"{f_count: 6}  {f_name}")
+
+    @classmethod
+    def compute_at_directory(cls, breakdown_dir: Path | str, counts_per_directory: dict[Path, int]) -> "Breakddown":
+        breakdown_dir = Path(breakdown_dir)
+
+        direct_children = list(breakdown_dir.glob("*"))
+        direct_child_dirs = [child for child in direct_children if child.is_dir()]
+        direct_child_files = [child for child in direct_children if child.is_file()]
+
+        # aggregate
+        counts_per_child_dir: dict[str, int] = {
+            str(d): counts_per_directory[str(d)] 
+                    if str(d) in counts_per_directory
+                    else 0
+            for d in direct_child_dirs
+        }
+        
+        counts_per_child_dir = dict(sorted(counts_per_child_dir.items(), key=lambda item: item[1], reverse=True))
+
+        return Breakddown(
+            counts_per_child_dir,
+            direct_child_files
+        )
+        
+
 def gather_counts_per_directory(root_dir: Path | str) -> dict[Path, int]:
     root_dir = Path(root_dir)
     assert root_dir.is_dir()
@@ -23,33 +64,8 @@ def gather_counts_per_directory(root_dir: Path | str) -> dict[Path, int]:
 
     return counts_per_directory
 
-def get_breatkdown_at_directory(breakdown_dir: Path | str):
-    breakdown_dir = Path(breakdown_dir)
-
-    direct_children = list(breakdown_dir.glob("*"))
-    direct_child_dirs = [child for child in direct_children if child.is_dir()]
-    direct_child_files = [child for child in direct_children if child.is_file()]
-
-    counts_per_directory = gather_counts_per_directory(breakdown_dir)
-
-    # aggregate
-    counts_per_child_dir: dict[str, int] = {
-        str(d): counts_per_directory[str(d)] 
-                if str(d) in counts_per_directory
-                else 0
-        for d in direct_child_dirs
-    }
-    
-    counts_per_child_dir = dict(sorted(counts_per_child_dir.items(), key=lambda item: item[1], reverse=True))
-
-    print(f"{breakdown_dir}:")
-    for d_name, d_count in counts_per_child_dir.items():
-        print(f"{d_count: 8}  {d_name}")
-    
-    f_count = len(direct_child_files)
-    f_name = "direct child files"
-    print(f"{f_count: 6}  {f_name}")
-
 
 if __name__ == "__main__":
-    get_breatkdown_at_directory(" ".join(sys.argv[1:]))
+    breakdown_dir = " ".join(sys.argv[1:])
+    counts_per_directory = gather_counts_per_directory(breakdown_dir)
+    Breakddown.compute_at_directory(breakdown_dir, counts_per_directory).to_stdout()
